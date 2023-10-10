9f4e44642a20cbee75329a29efdbc1e2728466ad
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-10 21:50:56 +0300

    WIP: Grouping messages by keywords

--- sources/views.py
+++ sources/views.py
@@ -7,13 +7,15 @@ from telethon.client import TelegramClient
 from asgiref.sync import async_to_sync
 import json
 
+from keywords.groupping import group_by_keywords
+from keywords.clearing import clear_text
+
 
 @require_GET
 @async_to_sync
 async def get_messages(request):
     channels = request.GET.getlist("channels[]")
     limit = request.GET.get("limit", 10)
-    articles = {}
     async with TelegramClient(
         "Gathering articles",
         settings.TELETHON["API_ID"],
@@ -21,8 +23,7 @@ async def get_messages(request):
     ) as client:
         for name in channels:
             messages = await gather_messages(client, name, limit)
-            articles[name] = [serialize_message(obj) for obj in messages]
-    data = json.dumps(articles, ensure_ascii=False).encode("utf8")
+    data = json.dumps(group_by_keywords(messages), ensure_ascii=False).encode("utf8")
     return HttpResponse(data, content_type="application/json; charset=utf-8")
 
 
@@ -30,12 +31,12 @@ async def gather_messages(client, name, limit):
     messages = []
     async for message in client.iter_messages(name, limit=limit):
         messages.append(message)
-    return messages
+    return clear_messages(messages)
 
 
-def serialize_message(message):
-    return {
-        "message": message.message,
-        "date": str(message.date),
-        "views": message.views,
-    }
+def clear_messages(messages):
+    result = []
+    for message in messages:
+        if message.message:
+            result.append(clear_text(message.message))
+    return result
