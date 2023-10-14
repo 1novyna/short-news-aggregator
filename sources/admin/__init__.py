6fb0dd171c6cd764df0fb3c673a3b5c782e1e7e5
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-14 11:39:33 +0300

    Make structurally telegram only source

--- sources/admin/__init__.py
+++
@@ -1,5 +0,0 @@
-from .telegram import TelegramChannelAdmin
-
-__all__ = [
-    "TelegramChannelAdmin",
-]
--- sources/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .telegram import TelegramChannel, TelegramMessage
-
-__all__ = [
-    "TelegramChannel",
-    "TelegramMessage",
-]
