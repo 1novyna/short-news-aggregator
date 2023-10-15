5f42f71670024c3fa4cc5feb9ac74ba762f6830e
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-15 18:18:06 +0300

    Add regular gathering messages

--- sources/apps.py
+++ sources/apps.py
@@ -1,6 +1,25 @@
 from django.apps import AppConfig
 
+from threading import Timer
+from asgiref.sync import async_to_sync
+
+
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
 
 class SourcesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sources"
+
+    def ready(self):
+        self.start_background_task()
+
+    def start_background_task(self):
+        from .tasks import gather_messages
+
+        thread = RepeatTimer(float(10 * 60), async_to_sync(gather_messages))
+        thread.daemon = True
+        thread.start()
