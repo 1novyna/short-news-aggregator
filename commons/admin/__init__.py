8a5074b7b1eec2c0c12abfa698459e6614f41294
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-16 03:08:19 +0300

    Add filtering on embedding in admin panel

---
+++ commons/admin/__init__.py
@@ -0,0 +1,8 @@
+from .tags import ExternalLinkTag
+from .filters import GeneralBooleanListFilter, bool_filter_factory
+
+__all__ = [
+    "ExternalLinkTag",
+    "GeneralBooleanListFilter",
+    "bool_filter_factory",
+]
