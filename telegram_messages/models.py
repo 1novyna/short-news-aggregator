0ca941b03441d51e786c9b4a594bfea2850e5476
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-28 21:35:53 +0300

    Move clusterize models to separate module

--- telegram_messages/models.py
+++ telegram_messages/models.py
@@ -1,42 +1,8 @@
 from django.db import models
 
-from sklearn.cluster import KMeans
-import numpy as np
-import pandas as pd
-
 from commons.models import EmbeddingField
 from telegram_channels.models import Channel
 from clusters.models import Cluster
-
-
-class MessageQuerySet(models.QuerySet):
-    def get_clusters(self):
-        df = self.to_data_frame()
-        n_clusters = self.get_approximate_clusters_number()
-
-        kmeans = KMeans(n_clusters=n_clusters, init="k-means++", random_state=42)
-        matrix = np.vstack(df.embedding.values)
-        kmeans.fit(matrix)
-
-        labels = kmeans.labels_
-        df["cluster"] = labels
-        df.groupby("cluster")
-
-        return df
-
-    def to_data_frame(self):
-        values = list(self.values())
-        return pd.DataFrame(values)
-
-    def get_approximate_clusters_number(self):
-        messages_number = self.aggregate(count=models.Count("id"))["count"]
-        channels_number = self.aggregate(
-            count=models.Count(
-                "channel_id",
-                distinct=True,
-            )
-        )["count"]
-        return messages_number // channels_number
 
 
 class Message(models.Model):
@@ -54,13 +20,11 @@ class Message(models.Model):
     embedding = EmbeddingField(blank=True, null=True)
     cluster = models.ForeignKey(
         Cluster,
-        models.RESTRICT,
+        models.SET_NULL,
         related_name="messages",
         blank=True,
         null=True,
     )
-
-    objects = MessageQuerySet.as_manager()
 
     def get_absolute_url(self):
         return f"https://www.t.me/{self.channel.username}/{self.message_id}"
